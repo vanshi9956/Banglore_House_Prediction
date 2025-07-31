e155248c4666035cd8716bb9194074c2df96c15d
Vanshika Jain <vanshi9956@gmail.com>
2025-07-31 15:25:14 +0530

    Initial commit: house price prediction with dataset

---
+++ client/app.js
@@ -0,0 +1,63 @@
+function getBathValue() {
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for (var i = 0; i < uiBathrooms.length; i++) {
+        if (uiBathrooms[i].checked) {
+            return parseInt(uiBathrooms[i].value);
+        }
+    }
+    return -1;
+}
+
+function getBHKValue() {
+    var uiBHK = document.getElementsByName("uiBHK");
+    for (var i = 0; i < uiBHK.length; i++) {
+        if (uiBHK[i].checked) {
+            return parseInt(uiBHK[i].value);
+        }
+    }
+    return -1;
+}
+
+function onClickedEstimatePrice() {
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("uiSqft");
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById("uiLocations");
+    var estPrice = document.getElementById("uiEstimatedPrice");
+
+    var url = "http://localhost:5000/api/predict_house_price";
+
+    $.post(url, {
+        total_sqft: parseFloat(sqft.value),
+        bhk: bhk,
+        bath: bathrooms,
+        location: location.value
+    }, function (data, status) {
+        console.log(data.estimated_price);
+        estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+        console.log(status);
+    });
+}
+
+function onPageLoad() {
+    console.log("document loaded");
+
+    var url = "http://localhost:5000/api/get_location_names";
+
+    $.get(url, function (data, status) {
+        console.log("got response for get_location_names request");
+        if (data) {
+            var locations = data.locations;
+            var uiLocations = document.getElementById("uiLocations");
+            $('#uiLocations').empty();
+            $('#uiLocations').append('<option value="" disabled selected>Choose a Location</option>');
+            for (var i = 0; i < locations.length; i++) {
+                var opt = new Option(locations[i], locations[i]);
+                $('#uiLocations').append(opt);
+            }
+        }
+    });
+}
+
+window.onload = onPageLoad;
